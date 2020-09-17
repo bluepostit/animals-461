dfc96ad268e7e7632af537bf1f9fc01a4aedca6c
Yair <32864499+bluepostit@users.noreply.github.com>
2020-09-17 17:48:09 +0300

    Lion#eat method, with test

--- lion.rb
+++ lion.rb
@@ -4,4 +4,17 @@ class Lion < Animal
   def talk
     "#{@name.capitalize} roars"
   end
+
+  def eat(food)
+    # NOTE: we can call `super` with no arguments!
+    # It will grab the arguments of the current
+    # method, and pass them to the parent class's version
+    # of the method.
+    #
+    # BUT if the parameters of the current method don't
+    # exactly match those of the parent class's version
+    # of the method, we must explicitly specify each
+    # argument.
+    "#{super}. It's the circle of life."
+  end
 end
--- spec/lion_spec.rb
+++ spec/lion_spec.rb
@@ -7,4 +7,12 @@ describe Lion do
       expect(simba.talk).to eq('Simba roars')
     end
   end
+
+  describe '#eat' do
+    it 'returns a sentence describing the eating' do
+      simba = Lion.new('simba')
+      description = "Simba eats a gazelle. It's the circle of life."
+      expect(simba.eat('a gazelle')).to eq(description)
+    end
+  end
 end
